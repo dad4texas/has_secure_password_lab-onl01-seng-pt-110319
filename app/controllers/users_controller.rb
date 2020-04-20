eb8a723ab77140cc21d959fec852ab8da7ca96c2
Warren Harper II <warrenharper2@att.net>
2020-04-20 15:22:03 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     def new
       @user = User.new
     end
-  
+
     def create
       @user = User.new(user_params)
       if @user.save
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
         redirect_to :new_user
       end
     end
-  
+
     private
     def user_params
         params.require(:user).permit(:username, :password, :password_confirmation)
