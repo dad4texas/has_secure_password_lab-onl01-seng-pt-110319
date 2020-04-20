054b1ec9fb68d955967e7878320a6963786d7973
Warren Harper II <warrenharper2@att.net>
2020-04-20 15:21:24 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     def new
       @user = User.new
     end
-  
+
     def create
       @user = User.find_by(name: params[:user][:name])
       if @user && @user.authenticate(params[:user][:password])
